d3a452a634489ccdd7f935fbb519646a544511aa
Chris Curvey <chris@chriscurvey.com>
2018-07-09 13:24:05 -0400

    removed log-dumping code

--- debugger_test.py
+++ debugger_test.py
@@ -12,6 +12,11 @@ import wingdbstub
 # set your breakpoint on the next line
 print("Stop at this breakpoint!")
 
-with open('/tmp/wing-docker-debug.log', 'r') as logfile:
-    for line in logfile:
-        print line,
+######################################################################
+## If you are having troubles with getting breakpoints to work, you can
+## get a lot of information by uncommenting these next lines **AND**
+## uncommenting the section labeled "BREAKDPOINT-DEBUGGING" in the
+## wingdbstub.py file that is included with this project.
+##with open('/tmp/wing-docker-debug.log', 'r') as logfile:
+##    for line in logfile:
+##        print line,
